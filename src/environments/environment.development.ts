4a8b775c246121789a4aabd05ec10fdb99d103a2
Rituraj Anand <riturajanand91@gmail.com>
2025-02-27 10:24:30 +0530

    env changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    baseUrl: 'https://www.optumflex.live:8000/fin/Dealer/End/User',
+    baseUrl: 'https://www.optumflex.live/fin/Dealer/End/User',
     endpoints: {
       verifyMail: '/verifyMail',
       dashboard: '/dashboard',
